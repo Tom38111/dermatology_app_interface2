6899bbcb793c560dd59e6192d7a07ba7952f069c
Thomas PELLETIER <thomaspelletier@hotmail.com>
2024-12-16 16:23:46 +0100

    new commitment

--- app/app.py
+++ app/app.py
@@ -4,6 +4,9 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
+image1 = Image.open('raw_data/dermatology_app.jng')
+st.image(image1, use_container_width=True)
+
 st.markdown("""# Dermatology app
 ## The application that help you to know if something is wrong on your skin
 Given a skin image, it will predict if it is  malignant or benign using a model of IA""")
@@ -14,10 +17,24 @@ st.image(image, caption='Melanoma check-up', use_container_width=True)
 
 with st.form(key='params_for_api'):
 
-    st.file_uploader('Please paste a skin image')
+    uploaded_file = st.file_uploader('Please paste a skin image')
 
     st.radio('Please select a model of IA', ['Convolutional Neural Network (CNN)','Pre-trained model'])
 
     st.markdown('''Please note that a dermatology app is not a diagnostic tool and cannot replace or substitute a visit to your doctor''')
 
     st.form_submit_button('Make prediction')
+
+
+if uploaded_file is not None:
+
+    params = uploaded_file
+
+    dermatology_app_api_url = 'https://dermatology_app_api.lewagon.ai/predict'
+    response = requests.get(dermatology_app_api_url, params=params)
+
+    prediction = response.json()
+
+    pred = prediction['???']
+
+    st.header(f'Fare amount: ${round(pred, 2)}')
