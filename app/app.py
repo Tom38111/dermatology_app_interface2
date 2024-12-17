580626f5d275f9220ce7f7e56564953fcdb80e4e
Thomas PELLETIER <thomaspelletier@hotmail.com>
2024-12-17 16:42:37 +0100

    OK

--- app/app.py
+++ app/app.py
@@ -23,10 +23,10 @@ st.markdown("""If you do not have a skin picture, drag and drop one of these ima
 
 with st.form(key='params_for_api'):
 
-    st.image((image2, image3, image4), width=150)
-    st.markdown("""If you do not have a skin picture, drag and drop one of these images""")
-
     uploaded_file = st.file_uploader('Please paste a skin image')
+
+    st.image((image2, image3, image4), width=100)
+    st.markdown("""If you do not have a skin picture, drag and drop one of these images""")
 
     model_type = st.radio('Please select a model of IA', ['Convolutional Neural Network (CNN) 1','Convolutional Neural Network (CNN) 2','Pre-trained model'])
 
